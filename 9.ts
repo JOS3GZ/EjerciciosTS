de973b95dbc8b1dace6caddc33a692df5dd902a7
Jose Miguel <jouse124@gmail.com>
2021-09-04 19:37:57 -0600

    Ejercicio 9

---
+++ 9.ts
@@ -0,0 +1,9 @@
+const nombres = ["Juan", "Pedro", "Luis"];
+
+nombres.forEach(function (s){
+   console.log(s.toUpperCase());
+});
+
+nombres.forEach((s) => {
+    console.log(s.toUpperCase());
+});
