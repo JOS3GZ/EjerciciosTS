86d084e89db47921fa9187bf65c6e72e5453768e
Jose Miguel <jouse124@gmail.com>
2021-09-04 19:29:14 -0600

    Ejercicio 8

---
+++ 8.js
@@ -0,0 +1,6 @@
+function elevarAlCuadraro(base) {
+    return base * base;
+}
+var numeroBase = 10;
+var numeroAlCuadrado = elevarAlCuadraro(numeroBase);
+console.log(numeroAlCuadrado);
---
+++ 8.ts
@@ -0,0 +1,7 @@
+function elevarAlCuadraro(base: number): number{
+    return base*base;
+}
+
+let numeroBase = 10;
+let numeroAlCuadrado = elevarAlCuadraro(numeroBase);
+console.log(numeroAlCuadrado);
