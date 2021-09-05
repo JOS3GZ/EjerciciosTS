38da870e45c71acaee4a3eefd3fe94059e323a0f
Jose Miguel <jouse124@gmail.com>
2021-09-04 18:55:47 -0600

    Ejercicio 6

---
+++ 6.js
@@ -0,0 +1,5 @@
+function imprimirId(id) {
+    console.log("El ID es  " + id);
+}
+imprimirId(1);
+imprimirId('abc');
---
+++ 6.ts
@@ -0,0 +1,6 @@
+function imprimirId(id:number | string){
+    console.log(`El ID es  ${id}`);
+}
+
+imprimirId(1);
+imprimirId('abc');
