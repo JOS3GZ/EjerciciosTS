08725c67f151adaf380553bd6c2754079de833e1
Jose Miguel <jouse124@gmail.com>
2021-09-04 19:03:31 -0600

    Ejercicio 6

--- 6.js
+++ 6.js
@@ -1,5 +1,17 @@
-function imprimirId(id) {
-    console.log("El ID es  " + id);
+/*function imprimirId(id:number | string){
+    console.log(`El ID es  ${id}`);
 }
+
 imprimirId(1);
 imprimirId('abc');
+*/
+function imprimirId(id) {
+    if (typeof id === "string") {
+        console.log("El ID es " + id.toUpperCase());
+    }
+    else {
+        console.log("El ID es " + id.toFixed(3));
+    }
+}
+imprimirId('este_es_mi_id');
+imprimirId(100.234234123);
--- 6.ts
+++ 6.ts
@@ -1,6 +1,19 @@
-function imprimirId(id:number | string){
+/*function imprimirId(id:number | string){
     console.log(`El ID es  ${id}`);
 }
 
 imprimirId(1);
 imprimirId('abc');
+*/
+
+function imprimirId(id:number | string){
+    if(typeof id === "string"){
+        console.log(`El ID es ${(id as string).toUpperCase()}`);
+    }else{
+        console.log(`El ID es ${(id as number).toFixed(2)}`);
+    }
+}
+
+imprimirId('este_es_mi_id');
+imprimirId(100.234234123);
+
