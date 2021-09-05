fc78a78f3662c4aee62ebeaa52ae502a5e0d9c94
Jose Miguel <jouse124@gmail.com>
2021-09-04 20:10:45 -0600

    Ejercicio 11

--- 11.ts
+++ 11.ts
@@ -1,6 +1,16 @@
-function imprimirEtiqueta(etiqueta:{label: string}){
-    console.log(etiqueta.label);
+interface Etiqueta{
+    label: string;
 }
 
+function imprimirEtiqueta(etiqueta: Etiqueta){
+    console.log(etiqueta.label);
+}
+ 
+
+/*function imprimirEtiqueta(etiqueta:{label: string}){
+    console.log(etiqueta.label);
+}*/
+
 let miEtiqueta = {numero: 10, label: "esta es mi etiqueta"};
-imprimirEtiqueta(miEtiqueta);
+imprimirEtiqueta(miEtiqueta)
+
